d051ebc99d1fa9f32ca0a77bc9d1e20bcbff5604
RuslanGon <ruslan441006@gmail.com>
2025-08-01 21:54:51 +0300

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer/Footer.jsx'
 import Header from './components/Header/Header.jsx'
 import Main from './components/Main/Main.jsx'
+import Coment from './pages/Coment/Coment.jsx'
 
 function App() {
-
-
   return (
-<div>
-  <Header />
-  <Main />
-  <Footer />
-</div>
+    <div className="appContainer">
+      <Header />
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/comments" element={<Coment />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
