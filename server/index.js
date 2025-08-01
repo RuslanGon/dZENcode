26e4a6a0f90aab1b42f95892d295cdf69dc7b94e
RuslanGon <ruslan441006@gmail.com>
2025-08-01 18:10:30 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+import cors from 'cors'
+import startServer from './db.js';
+
+
+dotenv.config(); 
 
 const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('✅ Server is running');
-});
 
-const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server started on port ${PORT}`);
-});
+
+startServer(app);
