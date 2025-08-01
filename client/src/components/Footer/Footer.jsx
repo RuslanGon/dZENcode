d92fd148e69e0bea28bcede617b9123c05af7b9b
RuslanGon <ruslan441006@gmail.com>
2025-08-01 22:32:42 +0300

    wer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.logo}>dZONcode</div>
+      <div className={styles.logo}>dZENcode</div>
       <p className={styles.text}>© 2025 dZONcode. Все права защищены.</p>
       <p className={styles.text1}>© Goncharenko Ruslan</p>
     </footer>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import styles from './Header.module.css'
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Link to="/" className={styles.logo}>dZONcode</Link>
+      <Link to="/" className={styles.logo}>dZENcode</Link>
       <nav className={styles.nav}>
         <Link to="/comments" className={styles.link}>Комментарии</Link>
         <Link to="/add-comment" className={styles.link}>Добавить комментарий</Link>
