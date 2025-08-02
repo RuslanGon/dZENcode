92fb1e92b3f07cd60d2a971b8b52685217e89353
RuslanGon <ruslan441006@gmail.com>
2025-08-02 16:12:06 +0300

    wer

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 import cors from 'cors'
 import startServer from './db.js';
 import { createPost, deletePost, getAllPosts, getByOnePost, uploadPost } from './controllers/PostContloller.js';
@@ -8,9 +7,12 @@ import { createPost, deletePost, getAllPosts, getByOnePost, uploadPost } from '.
 
 dotenv.config(); 
 
+
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: ['http://localhost:5173','https://dzencode-beta.vercel.app/'], 
+
+credentials: true}))
 
 // Post
 
